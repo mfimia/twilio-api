63d55c5c9c14c7e2e90db352b3a77d0a1477a297
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-11 18:39:09 +0100

    remove comments

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const accountSid = process.env.TWILIO_SID; // Your Account SID from www.twilio.com/console
-const authToken = process.env.TWILIO_TOKEN; // Your Auth Token from www.twilio.com/console
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_TOKEN;
 
 const client = new twilio(accountSid, authToken);
 
 client.messages
   .create({
     body: "Lorem ipsum",
-    to: process.env.END_NUM, // Text this number
-    from: process.env.TWILIO_TRIAL_NUM, // From a valid Twilio number
+    to: process.env.END_NUM,
+    from: process.env.TWILIO_TRIAL_NUM,
   })
   .then((message) => console.log(message.sid));
