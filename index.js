ab9f6af5fd4f73819efefd32325e884480fd0f30
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-11 18:35:09 +0100

    basic message deliver config

---
+++ index.js
@@ -0,0 +1,17 @@
+const twilio = require("twilio");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const accountSid = process.env.TWILIO_SID; // Your Account SID from www.twilio.com/console
+const authToken = process.env.TWILIO_TOKEN; // Your Auth Token from www.twilio.com/console
+
+const client = new twilio(accountSid, authToken);
+
+client.messages
+  .create({
+    body: "Hola compaps",
+    to: process.env.END_NUM, // Text this number
+    from: process.env.TWILIO_TRIAL_NUM, // From a valid Twilio number
+  })
+  .then((message) => console.log(message.sid));
