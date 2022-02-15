a62cd2ab6aadbbb2bc4744278143fd423a9857f1
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-15 20:23:59 +0100

    whatsapp trial

--- index.js
+++ index.js
@@ -8,10 +8,37 @@ const authToken = process.env.TWILIO_TOKEN;
 
 const client = new twilio(accountSid, authToken);
 
+// client.messages
+//   .create({
+//     body: "Lorem ipsum",
+//     to: process.env.END_NUM,
+//     from: process.env.TWILIO_TRIAL_NUM,
+//   })
+//   .then((message) => console.log(message.sid));
+
+// client.calls
+//   .create({
+//     url: "http://demo.twilio.com/docs/voice.xml",
+//     to: process.env.END_NUM,
+//     from: process.env.TWILIO_TRIAL_NUM,
+//   })
+//   .then((call) => console.log(call.sid));
+
+// client.calls.each((call) => console.log(call));
+// client.messages
+//   .list()
+//   .then((calls) => calls.forEach((call) => console.log(call.body)))
+//   .catch((err) => console.log(err));
+// client
+//   .calls("CA8a43c8acadbda437771e3f74f5b7caf4")
+//   .fetch()
+//   .then((call) => console.log(call));
+
 client.messages
   .create({
-    body: "Lorem ipsum",
-    to: process.env.END_NUM,
-    from: process.env.TWILIO_TRIAL_NUM,
+    body: "Lorem Ipsum",
+    from: process.env.WHATSAPP_TWILIO_TRIAL_NUM,
+    to: process.env.WHATSAPP_END_NUM,
   })
-  .then((message) => console.log(message.sid));
+  .then((message) => console.log(message.sid))
+  .done();
