12e8b613fb33b07d60860c0f22e5fc9f311f5ccd
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-16 16:18:52 +0100

    url set to localhost

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const client = new twilio(accountSid, authToken);
 //   })
 //   .then((message) => console.log(message.sid));
 
-// client.calls
-//   .create({
-//     url: "http://demo.twilio.com/docs/voice.xml",
-//     to: process.env.END_NUM,
-//     from: process.env.TWILIO_TRIAL_NUM,
-//   })
-//   .then((call) => console.log(call.sid));
+client.calls
+  .create({
+    url: "http://localhost:5000/voice",
+    to: process.env.END_NUM,
+    from: process.env.TWILIO_TRIAL_NUM,
+  })
+  .then((call) => console.log(call.sid));
 
 // client.calls.each((call) => console.log(call));
 // client.messages
@@ -34,11 +34,11 @@ const client = new twilio(accountSid, authToken);
 //   .fetch()
 //   .then((call) => console.log(call));
 
-client.messages
-  .create({
-    body: "Lorem Ipsum",
-    from: process.env.WHATSAPP_TWILIO_TRIAL_NUM,
-    to: process.env.WHATSAPP_END_NUM,
-  })
-  .then((message) => console.log(message.sid))
-  .done();
+// client.messages
+//   .create({
+//     body: "Lorem Ipsum",
+//     from: process.env.WHATSAPP_TWILIO_TRIAL_NUM,
+//     to: process.env.WHATSAPP_END_NUM,
+//   })
+//   .then((message) => console.log(message.sid))
+//   .done();
